abb0579ec635d16d6bd0b8f8fee8d62e4a578c6a
Matteo Gazzetta <matteo_gazzetta@hotmail.it>
2017-07-13 21:30:24 +0200

    Updated NN topology

--- src/main/scala/it/databiz/spark/deep/learning/MultiLayerConfigurationBuilder.scala
+++ src/main/scala/it/databiz/spark/deep/learning/MultiLayerConfigurationBuilder.scala
@@ -37,51 +37,23 @@ object MultiLayerConfigurationBuilder extends MultiLayerConfiguration.Builder {
       .seed(seed) // Random number generator seed. Used for reproducibility between runs.
       .iterations(iterations) // Number of optimization iterations.
       .regularization(true) // Whether to use regularization (L1, L2, dropout, etc...)
-      .l2(0.0005) // L2 regularization coefficient.
-      .learningRate(0.1) // Learning rate.
       .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT) // Optimization algorithm to use.
-      .updater(Updater.ADAGRAD) // Gradient updater.
+      .activation(Activation.LEAKYRELU)
+      .weightInit(WeightInit.XAVIER) // Weight initialization scheme. The Xavier algorithm automatically determines the scale of initialization based on the number of input and output neurons.
+      .learningRate(0.02) // Learning rate.
+      .updater(Updater.NESTEROVS) // Gradient updater
+      .momentum(0.9)
+      .regularization(true)
+      .l2(1e-4) // L2 regularization coefficient.
       .list()
-      .layer(
-        0,
-        new ConvolutionLayer.Builder(5, 5)
-          .nIn(numChannels) // Number of input neurons (channels).
-          .stride(1, 1)
-          .nOut(20) // Number of output neurons.
-          .weightInit(WeightInit.XAVIER) // Weight initialization scheme. The Xavier algorithm automatically determines the scale of initialization based on the number of input and output neurons.
-          .activation(Activation.RELU) // Activation function: rectified linear, an activation function defined as f(x) = max(0, x).
-          .build()
-      )
-      .layer(
-        1,
-        new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, Array(2, 2))
-          .build()
-      )
+      .layer(0, new DenseLayer.Builder().nIn(height * width).nOut(500).build())
+      .layer(1, new DenseLayer.Builder().nIn(500).nOut(100).build())
       .layer(
         2,
-        new ConvolutionLayer.Builder(5, 5)
-          .nIn(20) // Number of input neurons (channels).
-          .nOut(50) // Number of output neurons.
-          .stride(2, 2)
-          .weightInit(WeightInit.XAVIER) // Weight initialization scheme. The Xavier algorithm automatically determines the scale of initialization based on the number of input and output neurons.
-          .activation(Activation.RELU) // Activation function: rectified linear, an activation function defined as f(x) = max(0, x).
-          .build()
-      )
-      .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, Array(2, 2)).build)
-      .layer(
-        4,
-        new DenseLayer.Builder()
-          .activation(Activation.RELU) // Activation function: rectified linear, an activation function defined as f(x) = max(0, x).
-          .weightInit(WeightInit.XAVIER) // Weight initialization scheme. The Xavier algorithm automatically determines the scale of initialization based on the number of input and output neurons.
-          .nOut(200) // Number of output neurons.
-          .build()
-      )
-      .layer(
-        5,
         new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+          .activation(Activation.SOFTMAX)
+          .nIn(100) // Number of input neurons
           .nOut(outputNum) // Number of output neurons.
-          .weightInit(WeightInit.XAVIER) // Weight initialization scheme. The Xavier algorithm automatically determines the scale of initialization based on the number of input and output neurons.
-          .activation(Activation.SOFTMAX) // Activation function. The softmax function, or normalized exponential, is a generalization of the logistic function that "squashes" a K-dimensional vector of arbitrary real values to a K-dimensional vector of real values in the range (0, 1) that adds up to 1.
           .build()
       )
       .backprop(true) // Whether to use backpropagation.
